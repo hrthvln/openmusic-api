5a2ac7db787912b698eed1c6152f3c6c40101a73
belajardibumi <jelitasukawati13@gmail.com>
2025-06-11 07:49:50 +0700

    add songsservice.

--- src/api/playlists/index.js
+++ src/api/playlists/index.js
@@ -1,13 +1,14 @@
 // Plugin Hapi untuk pengelolaan playlist.
 
+const SongsService = require('../../services/postgres/SongsService');
 const PlaylistsHandler = require('./handler');
 const routes = require('./routes');
 
 module.exports = {
   name: 'playlists',
   version: '1.0.0',
-  register: async (server, { service, validator }) => {
-    const playlistsHandler = new PlaylistsHandler(service, validator);
+  register: async (server, { service, validator, songsService }) => {
+    const playlistsHandler = new PlaylistsHandler(service, validator, SongsService);
     server.route(routes(playlistsHandler));
   },
 };
